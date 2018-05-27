516cad189f2eb9268e389c203ae4e40b7d190e6e
MikkonenTS <mikkonen.ts@gmail.com>
2018-05-27 08:32:31 +0000

    Base templates

--- dashbase/dashbase/settings.py
+++ dashbase/dashbase/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'dashbase.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'dash/templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
